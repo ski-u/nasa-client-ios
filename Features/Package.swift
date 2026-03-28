2da6f3c73f98b65617e270eef6a8cfd02af7caac
Yunosuke Sakai <qmkns246@gmail.com>
2026-03-28 10:27:33 +0900

    deps: update `pointfreeco/swift-dependencies` to `1.12.0`

--- Features/Package.swift
+++ Features/Package.swift
@@ -59,7 +59,7 @@ let package = Package(
             url: "https://github.com/pointfreeco/swift-composable-architecture.git",
             from: "1.25.2"
         ),
-        .package(url: "https://github.com/pointfreeco/swift-dependencies.git", from: "1.11.0"),
+        .package(url: "https://github.com/pointfreeco/swift-dependencies.git", from: "1.12.0"),
         .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", from: "4.2.2"),
         .package(url: "https://github.com/konomae/swift-local-date.git", from: "0.5.0"),
     ],
